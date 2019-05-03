6a82d76817209a0b282d57f5fcaf7edfbff44cfa
Matthew Emes <matthew.emes@neudesic.com>
2019-05-02 19:26:51 -0700

    Who am I?

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Investigations...
+
+# Use the latest 2.x Google provider >= 2.5.1
+provider "google" {
+  version = "~> 2.0, >= 2.5.1"
+
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+data "google_client_openid_userinfo" "whoami" {}
+
+# Apply stage will only happen if there are resources to create
+resource "random_string" "rnd" {
+  length  = "4"
+  special = false
+}
+
+output "whoami" {
+  value = "${data.google_client_openid_userinfo.whoami.email}"
+}
